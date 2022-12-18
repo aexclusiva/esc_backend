606a7575473170bd7fa301dcaa2fec23d673fc79
dev <aexclusiva123@gmail.com>
2022-12-18 01:59:30 +0000

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require("./app");
 const start = async () => {
   try {
-    app.listen(3000, () => console.log("Server started on port 3000"));
+    app.listen(process.env.PORT || 3000, () => console.log("Server started on port 3000"));
   } catch (error) {
     console.error(error);
     process.exit(1);
